dd029b4ae1a00338ca696765b0829df1b697f13d
aditya <adityaray947@gmail.com>
2024-08-22 12:10:00 +0530

    build

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,13 +4,26 @@ import mongoose from "mongoose";
 import bookRoute from "./Route/book.route.js";
 import cors from "cors";
 import userRoute from "./Route/user.route.js";
-
+import path from "path"
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const app = express();
 app.use(cors());
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1,"/Frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.join(__dirname1, "Frontend","build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
 
 const corsOptions = {
   origin: process.env.CORS,
@@ -19,7 +32,6 @@ const corsOptions = {
 };
 
 app.use(express.json());
-
 
 app.use('/book', bookRoute); 
 app.use('/user',userRoute);
